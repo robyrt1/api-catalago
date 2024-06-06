e76a2bd30ede89e3226690de91dea275d617a86f
robyrt1 <robertpinto1190.rpm@gmail.com>
2024-06-06 08:01:28 -0400

    refactory: add mock de  para o teste de delete movie

---
+++ src/__tests__/__mocks__/movie/usecases/delete.movie.use.case.mock.ts
@@ -0,0 +1,17 @@
+export const mockedDeleteMovie = {
+    id: 1,
+    title: 'teste02',
+    director: 'teste02',
+    releaseDate: '2024-07-05T20:08:19.266Z',
+  };
+  
+  export const mockedMovie = {
+    id: 1,
+    title: 'teste',
+    director: 'teste',
+    releaseDate: '2024-06-05T20:08:19.266Z',
+    activated: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+  
